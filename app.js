cd031bcd30dfeb8f0efb95f5fc16abef114e5088
Javor Emir <emir.j2002@gmail.com>
2025-10-14 12:08:37 +0200

    #MTE-3 Remove tag urgent

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function addition(a, b) {
   return a + b;
 }
-//test push commits MTE-1  eje bosse comme gotaga bbbd
+//test push commitss MTE-1  eje bosse comme gotaga bbbd
 module.exports = { addition };
