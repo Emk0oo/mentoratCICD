d158d6311faa3ae03b8b2290bc9e4298fe13a99b
Javor Emir <emir.j2002@gmail.com>
2025-10-14 11:57:56 +0200

    #MTE-2 Work item 2h Développement "Correction du bug sur le module login"

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function addition(a, b) {
   return a + b;
 }
-//test push commit MTE-1  je bosse comme gotaga combi en jean wafv
+//test push commit MTE-1  je bosse comme gotaga
 module.exports = { addition };
