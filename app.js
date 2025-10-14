2e9727bae113ddc5643836e9e63bc9a93c130840
Javor Emir <emir.j2002@gmail.com>
2025-10-14 12:07:50 +0200

    #MTE-3 Priority Critical

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function addition(a, b) {
   return a + b;
 }
-//test push commit MTE-1  je bosse comme gotaga bbbbd
+//test push commit MTE-1  je bosse comme gotaga bbbd
 module.exports = { addition };
