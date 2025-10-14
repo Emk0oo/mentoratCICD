9cda00de5630e218fa7fe29a820d74d3907b1474
Javor Emir <emir.j2002@gmail.com>
2025-10-14 12:06:36 +0200

    #MTE-3 Comment "proute"

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function addition(a, b) {
   return a + b;
 }
-//test push commit MTE-1  je bosse comme gotaga bbbbc
+//test push commit MTE-1  je bosse comme gotaga bbbb
 module.exports = { addition };
