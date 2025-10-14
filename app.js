ccd302b66c106b8ba8acf37384fd099d8da47fb1
MelinaClouet <melina.clouet@my-digital-school.org>
2025-10-14 13:49:58 +0200

    test sonar

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 function addition(a, b) {
- // throw new Error('Not implemented');
+
   return a + b;
 }
-function error(){
-    throw new Error('Not implemented');
-}
-//error();
+
 //test pushs scommitss MTE-3
 module.exports = { addition };
