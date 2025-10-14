374d7d7d91886e52e630c07e4087f1fe28cde355
Javor Emir <emir.j2002@gmail.com>
2025-10-14 12:06:01 +0200

    #MTE-3 Assignee clouet.melina

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function addition(a, b) {
   return a + b;
 }
-//test push commit MTE-1  je bosse comme gotaga bbbb
+//test push commit MTE-1  je bosse comme gotaga bbbbc
 module.exports = { addition };
