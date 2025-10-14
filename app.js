9ac2072b467c00a9a04823cef513c95c7a96a0c7
Javor Emir <emir.j2002@gmail.com>
2025-10-14 12:03:38 +0200

    MTE-2 #Fixed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function addition(a, b) {
   return a + b;
 }
-//test push commit MTE-1  je bosse comme gotaga bbbcc
+//test push commit MTE-1  je bosse comme gotaga bbbc
 module.exports = { addition };
