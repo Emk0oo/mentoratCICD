f9433feca523311436b7f4e96bf15db0ce78b8ba
Javor Emir <emir.j2002@gmail.com>
2025-10-14 12:14:12 +0200

    blab

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function addition(a, b) {
+  throw new Error('Not implemented');
   return a + b;
 }
+function error(){
+    throw new Error('Not implemented');
+}
+error();
 //test pushs scommitss MTE-1ss  seje bosse comme gotaga bbbd
 module.exports = { addition };
